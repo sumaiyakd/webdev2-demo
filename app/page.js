6b21f52ee754d5cd3bfdef6a8365796bb05cd68f
sumaiyakd <sumaiya.khurshid@edu.sait.ca>
2024-09-13 16:01:19 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <main>
       <h1>Web Dev 2 Demos</h1>
-      <p>These are the demos for Web Dev 2</p>
+      <p>These are the demos for Web Dev 2!</p>
     </main>
   );
 }
